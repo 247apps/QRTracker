e8d4e6bf7f4c2efa754c27f5cb3b7f8930d38ff9
Biana Web Developer <it4craigslist@gmail.com>
2019-10-16 10:56:47 -0700

    contact page
    iframe to contact form

---
+++ contact.php
@@ -0,0 +1,71 @@
+<?php
+$params['action'] = 'contact';
+require_once('header.php');
+?>
+
+
+<div class="page-content">
+
+    <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
+        <ol class="breadcrumb page-breadcrumb pull-left">
+            <li>Contact Us</li>
+        </ol>
+        <div class="clearfix"></div>
+    </div>
+
+    <div id="">
+        <div class="row mbl">
+            <div class="col-md-12">
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <p>Send us your questions, comments, bug reports, etc.</p>
+
+                                <div class="panel panel-green">
+                                    <div class="panel-body pan">
+                                        
+                                        <iframe src="http://www.247apps.mobi/blog/qrtracker-contact-us-popup/" frameborder="0" height="500" style="width:100%;height:500px;" ></iframe>
+
+<!--                                        <form action="/dashboard/contact/send" method="POST" id="contact-form">
+                                            <input type="hidden" name="customer" value="75066">
+
+                                            <div class="form-body pal">
+                                                <div class="form-group">
+                                                    <label for="info">Message</label>
+                                                    <textarea name="message" id="message" rows="7" class="form-control"></textarea>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-actions">
+                                                <div class="col-md-offset-3 col-md-9">
+                                                    <button type="submit" class="btn btn-primary" id="message-send">Send</button>
+                                                    <em class="invalid" id="sent-error"><i class="fa fa-times"></i> Sorry, internal error. Please try again.</em>
+                                                    <span id="sent" style="display: none;"><i class="fa fa-check"></i> message sent</span>
+                                                </div>
+                                            </div>
+
+                                        </form>-->
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>You can also e-mail us at <a href="mailto:api@247apps.mobi" title="Send us your feedback">api@247apps.mobi</a>. We'll get back to you soon.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php
+require_once('footer.php');
